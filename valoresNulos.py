2fa5a9dd7826c860272f467c36b97ce5c42e67a6
RodrigoT-P <a01703521@tex.mx>
2024-04-15 19:29:05 -0600

    Practica de valores nulos

--- valoresNulos.py
+++ valoresNulos.py
@@ -17,4 +17,46 @@ valores_nulos=df.isnull().sum()
 #Quitar nulos de columna TARJETAS DE CREDITO
 df['tarjetas_credito'] = df['tarjetas_credito'].fillna(round(df['tarjetas_credito'].median(),1))
 valores_nulos=df.isnull().sum()
+#print(valores_nulos)
+
+#Eliminar nulos columna OTROS MEDIO
+df['otros_medios']=df['otros_medios'].fillna(6922148.759)
+valores_nulos=df.isnull().sum()
+#print(valores_nulos)
+
+#ELIMINAR NULOS EN LA COLUMNA SUBTOTAL VENTAS
+df['subtotal_ventas_alimentos_bebidas'] =df['subtotal_ventas_alimentos_bebidas'].fillna(method='ffill')
+valores_nulos=df.isnull().sum()
+#print(valores_nulos)
+
+#Eliminar nulos en la columna BEBIDAS
+df['bebidas'] =df['bebidas'].fillna(round(df['bebidas'].median(),1))
+valores_nulos=df.isnull().sum()
+#print(valores_nulos)
+
+#Eliminar nulos columna ALMACEN
+df['almacen'] =df['almacen'].fillna(method='bfill')
+valores_nulos=df.isnull().sum()
+#print(valores_nulos)
+
+#Eliminar duplicados de la columna panaderia
+df['panaderia'] =df['panaderia'].fillna(0)
+valores_nulos=df.isnull().sum()
+#print(valores_nulos)
+
+#Eliminar nulos en columna LACTEOS
+df['lacteos'] =df['lacteos'].fillna(0)
+valores_nulos=df.isnull().sum()
+#print(valores_nulos)
+
+#Eliminar nulos en columna carne
+df['carnes'] =df['carnes'].fillna(round(df['carnes'].median(),1))
+
+#Eliminar nulos en la columna verduleria_fruteria
+df['verduleria_fruteria'] =df['verduleria_fruteria'].fillna(method='bfill')
+
+#Elimar nulos en columna alimentos_preparados
+df['alimentos_preparados_rotiseria'] =df['alimentos_preparados_rotiseria'].fillna(method='ffill')
+
 print(valores_nulos)
+df.to_csv("Ventas_totales_limpio")
